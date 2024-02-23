a61d656aa878530a160347a920c07be20c43c8e5
Ricardinho Pereira <39959555+ricardopra@users.noreply.github.com>
2024-02-23 08:42:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,13 @@ function nextImage(){
     }
     document.getElementById("radio"+count).checked = true 
 }
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-160BJX85DD"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-160BJX85DD');
+</script>
